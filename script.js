01efa7a6e784f7e21a26fecb7bbd5d971ef18631
kickypip <kickypip@gmail.com>
2021-06-16 16:23:09 -0700

    added comments to code, fixed an issue with evaluation before second operand

--- script.js
+++ script.js
@@ -15,15 +15,17 @@ function multiply(a, b) {
 
 function divide(a, b) {
     if (isNaN(a / b)) {
-        return;
+        return; // prevents "NaN"
     }
     return a / b;
 }
 
 function operate(operator, a, b) {
+    // converts operands to floating-point
     a = parseFloat(a);
     b = parseFloat(b);
 
+    // clears operator
     if (input.currentOperator) {
         operatorHighlight.remove(input.currentOperator);
         input.secondOperandExists = false;
@@ -46,7 +48,7 @@ let decimalPoint = document.querySelector('#decimal-point');
 let plusMinus = document.querySelector('#plus-minus');
 let percentageEquivalent = document.querySelector('#percentage-equivalent');
 
-// adds event listeners to each item
+// adds event listeners to each item, based on their respective html class
 let btns = document.querySelectorAll('.btn');
 btns.forEach(btn => {
     btnBorderHighlight(btn);
@@ -77,12 +79,15 @@ let operatorHighlight = {
 
 function btnNumberPress(number) {
     number.addEventListener('click', function() {
+
+        // formatting for second operand
         if (input.currentOperator && !input.secondOperandExists) {
             display.innerText = '';
             operatorHighlight.remove(input.currentOperator);
             input.secondOperandExists = true;
         }
 
+        // special rules for when decimal point or plus-minus is pressed
         if (this === decimalPoint && display.innerText.includes('.')) {
             return;
         } else if (this === plusMinus) {
@@ -94,29 +99,39 @@ function btnNumberPress(number) {
             return;
         }
 
+        // prevents a case of "01"
         if (display.innerText == 0) {
             display.innerText = display.innerText.replace('0', '');
         }
 
+        // clears display if something was just calculated
         if (calculation.result) {
             display.innerText = this.innerText;
             calculation.result = null;
         } else {
             display.innerText += this.innerText;
         }
+
+        // ensures first input is stored in "a"
+        if (input.a && input.currentOperator) {
+            input.b = display.innerText;
+        } else {
+            input.a = display.innerText
+        }
     })
 }
 
 function btnOperatorPress(operator) {
     operator.addEventListener('click', function() {
+
+        // forces each operand pair to be evaluated
         if (input.currentOperator) {
             input.b = display.innerText;
             calculation.result = operate(input.currentOperator.innerText, input.a, input.b);
             display.innerText = calculation.result;
             input.a = calculation.result;
-        } else {
-            input.a = display.innerText;
         }
+
         input.currentOperator = this;
         operatorHighlight.create(input.currentOperator);
         input.secondOperandExists = false;
@@ -128,10 +143,9 @@ percentageEquivalent.addEventListener('click', function() {
 })
 
 equals.addEventListener('click', function() {
-    input.b = display.innerText;
     if (calculation.result === input.a || !input.currentOperator) {
-        return;
-    } else if (input.a && input.b) {
+        return; // prevents a calculation immediately after one was made
+    } else if (input.a && input.b) { // ensures two inputs
         calculation.result = operate(input.currentOperator.innerText, input.a, input.b);
         display.innerText = calculation.result;
         input.a = calculation.result;
@@ -139,7 +153,7 @@ equals.addEventListener('click', function() {
 });
 
 clear.addEventListener('click', function () {
-    if (input.currentOperator) { 
+    if (input.currentOperator) {
         operatorHighlight.remove(input.currentOperator);
     }
     clearData();
